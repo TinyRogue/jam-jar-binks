97eaae3bcf5f2467e1a61c9d05e95a10c00d1a53
Oskar Mikus <oskarmikus@protonmail.com>
2021-04-17 15:38:37 +0200

    PUBLIC FOR THE LIFE

---
+++ public/scripts/main_view.js
@@ -0,0 +1,28 @@
+function resizeGridItem(item){
+    const grid = document.getElementsByClassName("grid")[0];
+    const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    const rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+    item.style.gridRowEnd = "span "+ rowSpan;
+}
+
+function resizeAllGridItems(){
+    const allItems = document.getElementsByClassName("item");
+    for (let x = 0; x < allItems.length ; x++){
+        resizeGridItem(allItems[x]);
+    }
+}
+
+function resizeInstance(instance){
+    const item = instance.elements[0];
+    resizeGridItem(item);
+}
+
+window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
+
+allItems = document.getElementsByClassName("item");
+
+for(let x = 0; x < allItems.length ; x++){
+    imagesLoaded( allItems[x], resizeInstance);
+}
