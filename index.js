dd075a4af5c3d69194ef0f06e5d8bbd1f8214e18
Wixaxis <wixaxis@gmail.com>
2021-04-17 15:40:52 +0200

    dummy commit

--- index.js
+++ index.js
@@ -6,39 +6,40 @@ const app = express();
 const http = require('http').Server(app);
 const bcrypt = require('bcrypt');
 const hash = require('password-hash');
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const emailValidator = require("email-validator");
 const MongoClient = require('mongodb').MongoClient;
 
 function connectToDb() {
-  return new Promise(resolve => {
-    const uri = `mongodb+srv://cityAdmin:${process.env.MONGO_PASSWORD}@city.zzigb.mongodb.net/city?retryWrites=true&w=majority`;
-    const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-    client.connect(err => {
-      if (err) return resolve({err: err, db: null});
-      resolve({err: null, db: client.db("city")});
+    return new Promise(resolve => {
+        const uri = `mongodb+srv://cityAdmin:${process.env.MONGO_PASSWORD}@city.zzigb.mongodb.net/city?retryWrites=true&w=majority`;
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        client.connect(err => {
+            if (err) return resolve({ err: err, db: null });
+            resolve({ err: null, db: client.db("city") });
+        });
     });
-  });
 }
 
-(async () => {
-  const {err, db} = await connectToDb();
-  if (err) throw err;
+(async() => {
+    const { err, db } = await connectToDb();
+    if (err) throw err;
 
-  app.use(rateLimit({
-    windowMs: 60 * 1000 * 1,
-    max: 60
-  }));
-  app.use(express.urlencoded({extended: false}));
-  app.use(express.json());
+    app.use(rateLimit({
+        windowMs: 60 * 1000 * 1,
+        max: 60
+    }));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(__dirname + '/styles');
 
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/home.html')
-  });
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/views/home.html');
+    });
 
-  const PORT = process.env.PORT | 3000;
-  
-  http.listen(PORT, () => {
-    console.log('http server listening on port ' + PORT);
-  });
+    const PORT = process.env.PORT | 3000;
+
+    http.listen(PORT, () => {
+        console.log('http server listening on port ' + PORT);
+    });
 })();
