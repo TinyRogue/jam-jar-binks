71d7b75fdfd00902c7e3804b950cb3ed017e450a
Wiktor Kania <roller47@wp.pl>
2021-04-18 04:44:31 +0200

    fixed db

--- routes/addIdea.js
+++ routes/addIdea.js
@@ -2,6 +2,12 @@ const { v4: uuidv4 } = require('uuid');
 
 function getHandler(db) {
   return async function(req, res) {
+    if (!req.session.logged) {
+      return res.json({
+        success: false,
+        msg: 'not logged'
+      })
+    }
     const requiredFields = ['title', 'desc', 'address', 'city'];
     for (const field of requiredFields) {
       if (!req.body[field]) {
@@ -23,7 +29,10 @@ function getHandler(db) {
       title: req.body.title,
       desc: req.body.desc,
       address: req.body.address,
-      city: req.body.city
+      city: req.body.city,
+      addedBy: req.session.userID,
+      addedOn: new Date(),
+      comments: []
     }
     req.files.image.mv(__dirname + '/../public/uploads/' + imageID + '.png', async function() {
       await db.collection('ideas').insertOne(ideaData);
